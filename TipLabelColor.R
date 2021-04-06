246726d912a4050fecb1ee5c17cb4742d808309c
chrishenryfla <chrishenryfla@ucla.edu>
2021-04-05 17:29:06 -0700

    Update TipLabelColor.R
    Data Visualization - Plotting a phylogeny with tip colors based on a discrete character

--- TipLabelColor.R
+++ TipLabelColor.R
@@ -1,5 +1,8 @@
+#Set Working Directory
 setwd("~/Desktop/PhyloFolder")
 library(phytools)
+
+#Read in tre. file, labels/trait data associated with the tree and match the
 tree1 <- read.newick("pruned_tree_END.tre")
 labelz<-read.csv("labels_H.csv")
 TipsInOrder<-labelz[match(tree1$tip.label, labelz$X),]
@@ -12,16 +15,19 @@ tmp = factor(labelz$Hair)
 
 tmp = as.numeric(tmp)
 
+#Assigning colors for each character
 tmp = c("#000000","#FF0000FF")[tmp]
 
 Hair$Color<-tmp
 
 head(Hair)
 
+#Make pdf for plot
 pdf(file = "demo-pdf.pdf", height = 9, width = 8)
 plot(as.phylo(tree1), 
      tip.color=tmp,
      type='fan',label.offset=0.1,no.margin=TRUE, cex = .2, edge.width = .5)
 
+#Add Legend
 legend("bottomleft",title = "Hair Presence", c("N","Y"), fill = c("#000000","#FF0000FF"),horiz = TRUE, cex = .4)
 dev.off()
